356334289fc5f8b7c6844f974e167e652b1376db
Flawid DSouza <me@flawiddsouza.com>
2021-07-04 00:19:33 +0530

    fix: Modal > modal closes when a button inside is removed on click, since the element does not exist anymore in the dom

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -25,7 +25,11 @@ export default {
     },
     methods: {
         closeModalOnBackgroundClick(e) {
-            if(e.target.closest('.modal__content') === null) {
+            // document.body.contains(e.target) is needed when the clicked element is no longer in the DOM
+            // if you don't add it, the orphaned e.target element will close the modal, as its "closest" will
+            // not yield the .modal__content class element or any other elements for that matter
+            // because it has been removed by the user
+            if(e.target.closest('.modal__content') === null && document.body.contains(e.target)) {
                 this.$emit('update:modelValue', false)
             }
         }
