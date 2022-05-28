77099045f369e39eebe5a84d64efeded03ad43d8
Flawid DSouza <me@flawiddsouza.com>
2022-05-29 00:43:35 +0530

    fix: CheckboxMultiselect > match color to other inputs

--- src/components/CheckboxMultiselect.vue
+++ src/components/CheckboxMultiselect.vue
@@ -47,7 +47,7 @@ export default {
             rootStyle: {
                 display: 'inline-block',
                 padding: '.429rem .929rem',
-                border: '1px solid #e0e0e0',
+                border: '1px solid #767676',
                 'border-radius': '.215rem',
                 height: '12em',
                 'min-width': '10em',
